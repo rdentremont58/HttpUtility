757dd32178e3c42666c162b25b8bdee7b680101c
rdentremont58 <91749434+rdentremont58@users.noreply.github.com>
2021-10-01 16:16:28 -0400

    Initial Commit

---
+++ src/main/java/com/lucidworks/HttpUtility/HttpUtilityApplication.java
@@ -0,0 +1,13 @@
+package com.lucidworks.HttpUtility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpUtilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucidworks/HttpUtility/HttpUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucidworks.HttpUtility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpUtilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
